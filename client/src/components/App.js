4ebfedf27e07a16f5d8202e6b31bee4886881248
YAICI Nassim <iimox04@gmail.com>
2022-07-26 18:53:57 +0100

    Addin Lazy Loading To The Front

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./Style.css";
-import Home from "./pages/Home";
+
+/* import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard"; */
+
+const Home = React.lazy(() => import("./pages/Home"));
+
+const Login = React.lazy(() => import("./pages/Login"));
+
+const Dashboard = React.lazy(() => import("./pages/Dashboard"));
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='login' element={<Login />} />
-                <Route path='dashboard' element={<Dashboard />} />
-            </Routes>
+            <Suspense fallback={<div></div>}>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='login' element={<Login />} />
+                    <Route path='dashboard' element={<Dashboard />} />
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     );
 };
